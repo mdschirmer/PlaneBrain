16f72497c9075cffb6edf37736fd78b6f0ca9983
Luca <langleri@mgh.harvard.edu>
2024-07-30 11:18:09 -0400

    Added option to render frame around slices containing lesion segmentations. Changed method for displaying slices from subplots to single array.

--- planebrain.py
+++ planebrain.py
@@ -22,6 +22,7 @@ Completely white rows and columns are removed from the final image. The script a
     --b <brain_file> : (Optional) Brain segmentation NIfTI file.
     --o <output_file> : Output image file name (default: file.png).
     --s <slice_step> : (Optional) Show only every nth slice (default: 2).
+    --g <guide> : (Optional) Draw border around slices containing lesion overlays.
     --smin <min_slice> : (Optional) Minimum slice number.
     --smax <max_slice> : (Optional) Maximum slice number.
     --dpi <dpi> : (Optional) Set output image dpi (default: 80).
@@ -35,13 +36,13 @@ Completely white rows and columns are removed from the final image. The script a
 #=============================================
 # Metadata
 #=============================================
-__author__ = ''
+__author__ = 'MDS, Refactored by LAA'
 __organization__ = ''
 __contact__ = ''
 __copyright__ = 'CC-BY'
 __license__ = 'http://www.opensource.org/licenses/mit-license.php'
 __date__ = '2024-05-21'
-__version__ = '0.4'
+__version__ = '0.5'
 
 #=============================================
 # Import statements
@@ -146,6 +147,31 @@ def read_dicom_folder(dcmpath):
 
     return ArrayDicom
 
+def draw_frame(rows, cols, fill_value):
+    border_width = 4
+    canvas = np.zeros((rows, cols)) #nans are ignored in plt.imshow() allowing for colored frame with transparent center 
+    canvas[:, 0:int(border_width/2)] = fill_value
+    canvas[:, -int(border_width/2):] = fill_value
+    canvas[0:border_width, :] = fill_value
+    canvas[-border_width:, :] = fill_value
+    return canvas
+
+def generate_bounds(stacked, row_idx, col_idx, slice_row_size, slice_col_size):
+    if stacked == True:
+        slice_0_r_lb = int(2*row_idx*slice_row_size)
+        slice_0_r_ub = int((row_idx*2+1)*slice_row_size) #first row upper bound, second row lower bound
+        slice_1_r_ub = int((row_idx*2+2)*slice_row_size) #second row upper bound
+        slice_c_lb = int(col_idx*slice_col_size) #column bounds only need to be calculated once
+        slice_c_ub = int((col_idx+1)*slice_col_size)
+        return [slice_0_r_lb, slice_0_r_ub, slice_1_r_ub, slice_c_lb, slice_c_ub]
+    else:
+        slice_0_r_lb = int(row_idx*slice_row_size)
+        slice_0_r_ub = int((row_idx+1)*slice_row_size) #first row upper bound
+        slice_1_r_ub = 0 #place holder
+        slice_c_lb = int(col_idx*slice_col_size) #column bounds only need to be calculated once
+        slice_c_ub = int((col_idx+1)*slice_col_size)
+        return [slice_0_r_lb, slice_0_r_ub, slice_1_r_ub, slice_c_lb, slice_c_ub]
+
 #=============================================
 # Main method
 #=============================================
@@ -212,10 +238,75 @@ def main(argv):
             # Calculate the number of rows needed
             if seg is not None or brain is not None:
                 rows_needed = (num_slices + img_per_row - 1) // img_per_row * 2  # Double the number of rows if segmentation overlays are present
+                stacked = True
             else:
                 rows_needed = (num_slices + img_per_row - 1) // img_per_row
+                stacked = False
+            
+            # Render figure as an array based on number of rows needed and images per row. Assumes all slices have same row x column dimensions
+            slice_row_size, slice_col_size = get_slice(img_vol, img_slices[0]).shape # Gets pixel dimensions of each slice
+            figure_array = np.zeros((3, slice_row_size*rows_needed, slice_col_size*img_per_row)) #Makes an array large enough to hold all frames
+            frame = draw_frame(slice_row_size, slice_col_size, 255) #renders frame once
+            l_w = 1 # Lineweight modifier for outlines. Final lineweight in pix is 1+2*l_w 
 
-            # Adjust figure size to minimize white space and ensure good visibility
+            for ii, img_slice in enumerate(img_slices):
+                # Generate upper and lower bounds for each slice's position in the array:
+                row_idx = ii // img_per_row
+                col_idx = ii % img_per_row
+                bounds = generate_bounds(stacked= stacked, row_idx=row_idx, col_idx=col_idx, slice_row_size= slice_row_size, slice_col_size= slice_col_size)
+                
+                # Insert base slices into first layer 
+                base_slice = get_slice(img_vol, img_slice) 
+                figure_array[0][bounds[0]:bounds[1], bounds[3]:bounds[4]] = base_slice 
+                
+                 
+                # Insert second row of slices if applicable
+                if seg is not None or brain is not None: 
+                    figure_array[0][bounds[1]:bounds[2], bounds[3]:bounds[4]] = base_slice
+                
+                # Generate lesion mask and add border to second layer
+                if seg is not None: 
+                    contours = get_outline(seg, img_slice)
+                    lesion_mask = np.zeros((base_slice.shape))
+                    for c in contours:
+                        for p in c: 
+                            lesion_mask[int(p[0]-l_w):int(p[0]+l_w), int(p[1]-l_w):int(p[1]+l_w)] = 1 
+                        if argv.g is True:
+                            figure_array[1][bounds[1]:bounds[2], bounds[3]:bounds[4]] = lesion_mask + frame 
+                        else:
+                            figure_array[1][bounds[1]:bounds[2], bounds[3]:bounds[4]] = lesion_mask
+
+                # Generate brain mask and add to third layer.
+                if brain is not None: 
+                    contours = get_outline(brain, img_slice)
+                    brain_mask = np.zeros((base_slice.shape))
+                    for c in contours:
+                        for p in c:
+                            brain_mask[int(p[0]-l_w):int(p[0]+l_w), int(p[1]-l_w):int(p[1]+l_w)]  = 255
+                    figure_array[2][bounds[1]:bounds[2], bounds[3]:bounds[4]] = brain_mask 
+
+            # Generate masked arrays to display figure properly
+            masked_lesion = np.ma.masked_where(figure_array[1] == 0, figure_array[1]) 
+            masked_brain = np.ma.masked_where(figure_array[2] == 0, figure_array[2])
+
+            # Display base layer with brain scans
+            plt.imshow(figure_array[0], cmap= 'gray') 
+            
+            # Display lesion or brain overlays if available:
+            if seg is not None:
+                plt.imshow(masked_lesion, cmap= 'autumn', interpolation='none') # In autumn cmap, low values are red and high are yellow. 
+
+            if brain is not None:
+                plt.imshow(masked_brain, cmap= 'cool', interpolation='none')
+
+            #Output figure
+            plt.axis('off')
+            plt.savefig(output_file, dpi = 1000, pad_inches = 0, bbox_inches = 'tight') 
+            
+
+                
+            """    
+            # Adjust figure size to minimize white space and ensure gsizeood visibility
             fig_width = img_per_row * 3  # Increase width to reduce thin columns
             fig_height = fig_width * (rows_needed / img_per_row)  # Calculate height based on the ratio of total slices to images per row
             fig = plt.figure(figsize=(fig_width, fig_height))
@@ -223,6 +314,7 @@ def main(argv):
             # Create axes for plotting and remove white space/axis where possible
             ax = gridspec.GridSpec(rows_needed, img_per_row, wspace=0, hspace=0)
 
+            
             for ii in range(rows_needed * img_per_row):
                 a = plt.subplot(ax[ii])
                 a.axis('off')
@@ -240,18 +332,25 @@ def main(argv):
                     # Plot segmentation overlays in even rows if available
                     overlay_row_idx = row_idx * 2 + 1
                     a = plt.subplot(ax[overlay_row_idx, col_idx])
-                    a.imshow(get_slice(img_vol, img_slice), cmap='gray')
+                    segmentation_slice = get_slice(img_vol, img_slice)
+                    slice_rows, slice_cols = segmentation_slice.shape
+                    a.imshow(segmentation_slice, cmap= 'gray')
+                    #a.imshow(get_slice(img_vol, img_slice), cmap='gray')
                     a.axis('off')
 
+                    contrast_multpilier = 50 #Multiplies contours to increase brightnes. 50 is arbitrary. 
                     if brain is not None:
-                        contours = get_outline(brain, img_slice)
+                        contours = get_outline(brain, img_slice) * contrast_multpilier
                         for contour in contours:
-                            a.plot(contour[:, 1], contour[:, 0], linewidth=.5, color='orange')
-
+                            a.plot(contour[:, 1], contour[:, 0], linewidth=.65, color='aqua') 
+         
                     if seg is not None:
-                        contours = get_outline(seg, img_slice)
+                        contours = get_outline(seg, img_slice) * contrast_multpilier
                         for contour in contours:
-                            a.plot(contour[:, 1], contour[:, 0], linewidth=.5, color='r')
+                            a.plot(contour[:, 1], contour[:, 0], linewidth=.65, color='red')
+                            if argv.g is True:
+                                frame = draw_frame(slice_rows, slice_cols, np.max(segmentation_slice))
+                                a.imshow(frame, cmap= "autumn")
                 else:
                     # Plot brain slices when no segmentations are available
                     a = plt.subplot(ax[row_idx, col_idx])
@@ -270,6 +369,7 @@ def main(argv):
             # Convert trimmed array back to image and save
             trimmed_img = Image.fromarray(trimmed_data)
             trimmed_img.save(output_file)
+            """
     except Exception as e:
         # Write the command that was used to call the script to failed.log
         output_dir = os.path.dirname(argv.o)
@@ -280,6 +380,7 @@ def main(argv):
             log_file.write(traceback.format_exc())
             log_file.write(f"#############################\n")
 
+
 if __name__ == "__main__":
     # Catch input
     try:
@@ -289,6 +390,7 @@ if __name__ == "__main__":
         parser.add_option('--b', dest='b', help='Brain segmentation FILE', metavar='FILE', default=None)
         parser.add_option('--o', dest='o', help='Output FILE', metavar='FILE', default='file.png')
         parser.add_option('--s', dest='s', help='Show only every s slice (default = 2)', metavar='INTEGER', default=None)
+        parser.add_option('--g', action='store_true', dest='g', help='Display frame around image slices containing lesion segmentations.', metavar = 'BOOL', default=False)
         parser.add_option('--smin', dest='smin', help='Minimum slice number', metavar='INTEGER', default=None)
         parser.add_option('--smax', dest='smax', help='Maximum slice number', metavar='INTEGER', default=None)
         parser.add_option('--dpi', dest='dpi', help='Set output image dpi', metavar='INTEGER', default=80)
